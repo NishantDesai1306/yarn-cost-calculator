6e5c5f5cc853cbfa34ac0ec50ebd75b2a3646a4c
Nishant Desai <nishantdesai1306@gmail.com>
2021-11-15 10:54:51 +0530

    cleared dependencies + integrated new workbox + build

--- workbox-config.js
+++ workbox-config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  "globDirectory": "public/",
-  "globPatterns": [
-    "**/*.{json,ico,png,html,js,css,svg,jpg}"
-  ],
-  "swDest": "public\\sw.js",
-  "swSrc": "src\\sw.js",
+	globDirectory: 'public/',
+	globPatterns: [
+		'**/*.{ico,png,css,html,json,js}'
+	],
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	],
+	swDest: 'public/sw.js'
 };
