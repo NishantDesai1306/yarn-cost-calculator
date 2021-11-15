934775b8f76cfcaada34af2b895505ea060e6a8f
Nishant Desai <nishantdesai1306@gmail.com>
2021-11-15 11:46:16 +0530

    added remove warp/weft functionality

--- src/Warps/warp-form.js
+++ src/Warps/warp-form.js
@@ -1,4 +1,4 @@
-import { Box, Card, CardContent, makeStyles, TextField, Typography } from '@material-ui/core';
+import { Box, Card, CardContent, makeStyles, TextField, Typography, Icon } from '@material-ui/core';
 import React, { useCallback } from 'react';
 
 const useStyles = makeStyles((theme) => {
@@ -35,9 +35,16 @@ export default function WarpForm({ name, warp, onChange, onRemove }) {
 	return (
 		<Card elevation={0} className="border">
 			<CardContent>
-				<Typography className={classes.title} color="textSecondary" gutterBottom>
-					{name}
-				</Typography>
+				<Box className="d-flex justify-content-between">
+					<div>
+						<Typography className={classes.title} color="textSecondary" gutterBottom>
+							{name}
+						</Typography>
+					</div>
+					<div className="text-muted cursor-pointer" onClick={onRemove}>
+						<Icon fontSize="small">close</Icon>
+					</div>
+				</Box>
 
 				<Box className="mt-3">
 					<div className="mb-3">
